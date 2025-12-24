646b3fec72ca93bce879bac3d9a230f44b5bdafc
AIGERIMM09 <aigerimformicrosoft@gmail.com>
2025-12-24 21:19:17 +0600

    Final Guess Game 1-100 with replay
    -изменен диапазон чисел до 100
    -добавлен цикл перезапуска игры
    -исправлены ошибки со скобками и предупреждения С#
    -настроены цвета консоли для победы и проигрыша

--- Program.cs
+++ Program.cs
@@ -1,29 +1,81 @@
-﻿
-    using System;
-
-
-    Console.WriteLine("--- Мини-калькулятор возраста ---");
-
-Console.Write("Как тебя зовут? ");
-string name = Console.ReadLine();
-
-Console.Write("В каком году вы родились? ");
-int birthYear =
-Convert.ToInt32(Console.ReadLine());
-
-int currentYear = DateTime.Now.Year;
-int age = currentYear - birthYear;
-
-Console.WriteLine($"{name}, тебе примерно {age} лет!");
-if (age < 18)
+﻿using System;
+    
+    Random random = new Random();
+bool playAgain = true;
+while (playAgain)
 {
-    Console.WriteLine("Ого, ты еще совсем молодая оказываеться, отлично мне посебедовать с молодым разработчиком, всё впереди! У тебя отлично получаеться!");
-}
-else
-{
-    Console.WriteLine("Ты уже взрослый и самостоятельный человек! Здорово! Рад познокомиться!Хорошего дня!");
-}    
-Console.ReadKey();
+    int secretNum = random.Next(1, 101);
+
+    Console.WriteLine("--- игра: Угадай число от 1 до 100 ---");
+    // и так далее...
+    int userGuess = 0;
+    int count = 0;
+    int maxAttempts = 10; //
+    do
+    {
+        Console.ResetColor(); // Сбрасываем цвет в начале каждого круга
+        Console.WriteLine($"\nУ тебя осталось попыток: {maxAttempts - count}");
+        // 2. Спрашиваем ответ у игрока
+        Console.Write("Введи свой вариант: ");
+        userGuess =
+            Convert.ToInt32(Console.ReadLine());
+        count++;
+
+        // 3. Сравниваем числа с подсказками
+        if (userGuess == secretNum)
+        {
+            Console.ForegroundColor =
+            ConsoleColor.Yellow; // Сделает текст золотым
+
+            Console.WriteLine($"УРА! Ты БОЛЬШАЯ молодец, угадала за {count} попыток! Это было число {secretNum}");
+            break;
+        }
+        else if (userGuess < secretNum)
+        {
+            Console.ForegroundColor =
+            ConsoleColor.Cyan; // Голубой цвет      
+            Console.WriteLine($"К сожелению не угадала...Моё число БОЛЬШЕ, чем {userGuess}");
+        }
+        else
+        {
+            Console.ForegroundColor =
+            ConsoleColor.Red; // Красный цвет
+            Console.WriteLine($"Жаль... Ты не угадала... Моё число МЕНЬШЕ, чем {userGuess}");
+        }
+        if (count >= maxAttempts && userGuess != secretNum)
+        {
+            Console.ForegroundColor =
+                ConsoleColor.Magenta; // Фиолетовый цвет
+            Console.WriteLine($"\nПОПЫТКИ ЗАКОНЧИЛИСЬ! ТЫ ПРОИГРАЛА...Жаль! На самом деле я загадала число {secretNum}");
+        }
+    } while (userGuess != secretNum);
+
+    Console.ResetColor();
+    Console.Write("\nХочешь сыграть еще раз? (д/н): ");
+    string? answer =
+    Console.ReadLine()?.ToLower();
+
+    if (answer == "д") //Проверяем ответ "д"
+    {
+        count = 0;    // Сбрасываем счетчик новой для новой игры
+        Console.Clear(); // Очищаем  экран
+    }
+    else
+    { 
+        playAgain = false; // Останавливаем цикл, если Не нажали "д"
+        Console.WriteLine("Спасибо за игру!");
+    }
+   } //
+
+Console.WriteLine("\nНажми любую клавишу, чтобы выйти...");
+    Console.ReadKey();
+
+
+
+
+
+
+    
 
 
 
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8b95be3e8cd49dd687c1b9daebaf2da2a3be8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
